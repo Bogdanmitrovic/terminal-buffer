39e427688d69af1e6f08b7e928486b1d9435b346
Bogdan Mitrović <bogdanmitrovic17102001@gmail.com>
2026-03-13 00:06:16 +0100

    initial structure for Line and TerminalBuffer classes, more to be added

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,4 +1,8 @@
 package org.bogdanmitrovic
 
-class Line {
+class Line(val width: Int) {
+    // for now buffer will be a list of lines, that way whole lines can go into history
+    // I will see if it needs changing if it makes more sense
+    val cells: Array<Cell> = Array(width) { Cell() }
+
 }
--- src/main/kotlin/TerminalBuffer.kt
+++ src/main/kotlin/TerminalBuffer.kt
@@ -1,4 +1,24 @@
 package org.bogdanmitrovic
 
 class TerminalBuffer {
+    var width:Int = 80
+    var height:Int = 24
+    var scrollbackLines:Int =10000 // only value I could find, default for ubuntu
+
+    var foregroundColor: Color = Color.DEFAULT
+    var backgroundColor: Color = Color.DEFAULT
+    var styles: Set<Style> = emptySet()
+
+    fun setup(newWidth:Int, newHeight:Int, newScrollbackLines:Int) {
+        width = newWidth
+        height = newHeight
+        scrollbackLines = newScrollbackLines
+    }
+
+    fun setAttributes(newForegroundColor: Color, newBackgroundColor: Color, newStyles: Set<Style>) {
+        foregroundColor = newForegroundColor
+        backgroundColor = newBackgroundColor
+        styles = newStyles
+    }
+
 }
