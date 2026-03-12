92ae8a703b8c87dd8d1389305dfdf294fea89e51
Bogdan Mitrović <bogdanmitrovic17102001@gmail.com>
2026-03-12 23:36:47 +0100

    Cell.kt now contains Cell class and Style and Color enums, for now

--- src/main/kotlin/Cell.kt
+++ src/main/kotlin/Cell.kt
@@ -1,4 +1,18 @@
 package org.bogdanmitrovic
 
+enum class Color {
+    DEFAULT, BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE,
+    BRIGHT_BLACK, BRIGHT_RED, BRIGHT_GREEN, BRIGHT_YELLOW, BRIGHT_BLUE,
+    BRIGHT_MAGENTA, BRIGHT_CYAN, BRIGHT_WHITE
+}
+
+enum class Style {
+    BOLD, ITALIC, UNDERLINE
+}
+
 class Cell {
+    val character: Char = ' '
+    val foregroundColor: Color = Color.DEFAULT
+    val backgroundColor: Color = Color.DEFAULT
+    val styles: Set<Style> = emptySet()
 }
