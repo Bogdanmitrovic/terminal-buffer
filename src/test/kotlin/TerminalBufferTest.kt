eb88c79aed38108ab43c208127ef046760ff00a5
Bogdan Mitrović <bogdanmitrovic17102001@gmail.com>
2026-03-14 13:52:38 +0100

    test: writeText tests added

--- src/test/kotlin/TerminalBufferTest.kt
+++ src/test/kotlin/TerminalBufferTest.kt
@@ -1,4 +1,5 @@
 import org.bogdanmitrovic.TerminalBuffer
+import java.awt.SystemColor.text
 import kotlin.test.*
 
 class TerminalBufferTest {
@@ -28,9 +29,9 @@ class TerminalBufferTest {
         buf.moveCursor(horizontal = -1)
         assertEquals(0, buf.cursorColumn)
         buf.moveCursor(vertical = buf.height)
-        assertEquals(buf.height-1, buf.cursorRow)
+        assertEquals(buf.height - 1, buf.cursorRow)
         buf.moveCursor(horizontal = buf.width)
-        assertEquals(buf.width-1, buf.cursorColumn)
+        assertEquals(buf.width - 1, buf.cursorColumn)
     }
 
     @Test
@@ -45,4 +46,49 @@ class TerminalBufferTest {
         buf.moveCursorLeft(4)
         assertEquals(6, buf.cursorColumn)
     }
+
+    @Test
+    fun `writeText writes text`() {
+        val buf = TerminalBuffer()
+        buf.writeText("test test")
+        assertEquals("test test", buf.getLine(0))
+    }
+
+    @Test
+    fun `writeText moves cursor`() {
+        val buf = TerminalBuffer()
+        buf.writeText("test")
+        assertEquals(0, buf.cursorRow)
+        assertEquals(4, buf.cursorColumn)
+    }
+
+    @Test
+    fun `writeText wraps cursor`() {
+        val buf = TerminalBuffer(5, 5)
+        buf.writeText("test test")
+        assertEquals(1, buf.cursorRow)
+        assertEquals(4, buf.cursorColumn)
+    }
+
+    @Test
+    fun `writeText overwrites content`() {
+        val buf = TerminalBuffer()
+        buf.writeText("test test")
+        buf.setCursorPosition(0, 0)
+        buf.writeText("abcdef")
+        assertEquals("abcdefest", buf.getLine(0))
+    }
+
+    @Test
+    fun `writeText scrolls up`() {
+        val buf = TerminalBuffer(5, 2)
+        for (i in 0..<3) {
+            buf.writeText("line$i")
+        }
+        assertEquals("line0", buf.getLine(0))
+        assertEquals("line1", buf.getLine(buf.scrollbackSize - 1))
+        assertEquals(2, buf.scrollbackSize)
+    }
+
+    //@Test
 }
